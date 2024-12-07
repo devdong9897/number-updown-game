ba424936db2759b878a16f3b9f293fe0a6050093
devdong9897 <dev.dong9897@gmail.com>
2024-12-07 22:34:48 +0900

    feat: 찬스기능 추가

--- main.js
+++ main.js
@@ -4,22 +4,41 @@ let playButton = document.getElementById("play-button");
 let resetButton = document.getElementById("reset-button");
 let chanceArea = document.getElementById("chance-area");
 let gameOver = false;
+let chances = 5;
+let history = [];
 
 let computer = 0;
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
 
 function random() {
   computer = Math.floor(Math.random() * 100) + 1;
+  console.log(computer);
 }
 
 function play() {
   const userValue = userInput.value;
 
+  if (userValue < 1 || userValue > 100) {
+    alert("1과 100사이의 숫자만 입력해주세요");
+    return;
+  }
+
+  if (history.includes(userValue)) {
+    alert("이미 입력한 숫자입니다.");
+    return;
+  }
+
   let imgElement = document.createElement("img");
   imgElement.style.width = "300px";
   imgElement.style.height = "200px";
+
+  chances--;
+  chanceArea.textContent = `남은 기회: ${chances}`;
 
   if (userValue < computer) {
     imgElement.src = "images/up.gif";
@@ -35,7 +54,19 @@ function play() {
     resultArea.appendChild(imgElement);
     resultArea.textContent = "맞췄습니다!!!";
   }
+
+  history.push(userValue);
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver === true) {
+    playButton.disabled = true;
+  }
 }
+
+random();
 
 function reset() {
   userInput.value = "";
